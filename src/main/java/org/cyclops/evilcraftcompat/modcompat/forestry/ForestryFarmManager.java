247974e1d58b6e14501c934c6661f8e468012794
GityUpNow <kim-cheng@gmx.de>
2018-01-22 19:21:27 +0100

    Updated the forestry version to the latest release and readded the undead sapling to the tree farm

---
+++ src/main/java/org/cyclops/evilcraftcompat/modcompat/forestry/ForestryFarmManager.java
@@ -0,0 +1,32 @@
+package org.cyclops.evilcraftcompat.modcompat.forestry;
+
+import forestry.api.core.ForestryAPI;
+import forestry.api.farming.IFarmRegistry;
+import forestry.farming.logic.FarmableSapling;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import org.cyclops.evilcraft.Configs;
+import org.cyclops.evilcraft.block.UndeadSaplingConfig;
+
+/**
+ * Registers Evilcraft-Plants in the Forestry Multifarm
+ * @author GityUpNow
+ */
+public class ForestryFarmManager {
+
+
+    public static void register() {
+        IFarmRegistry registry = ForestryAPI.farmRegistry;
+
+        //Add the Undead Sapling to the Tree Farm
+        if(Configs.isEnabled(UndeadSaplingConfig.class)) {
+
+            registry.registerFarmables("farmArboreal", new FarmableSapling(
+                    new ItemStack(UndeadSaplingConfig._instance.getBlockInstance()),
+                    new ItemStack[]{
+                            new ItemStack(Blocks.DEADBUSH)
+                    }
+            ));
+        }
+    }
+}
