6e44f213ef6a346d8dcae26ccbdeb75974ab3bf5
Ruben Taelman <rubensworks@gmail.com>
2018-07-01 13:49:03 +0200

    Bump CyclopsCore

--- src/main/java/org/cyclops/evilcraftcompat/Reference.java
+++ src/main/java/org/cyclops/evilcraftcompat/Reference.java
@@ -33,7 +33,7 @@ public class Reference {
     public static final String MOD_FORGE_VERSION_MIN = "14.23.1.2555";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
-    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.10.9";
+    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.11.6";
     public static final String MOD_EVILCRAFT = "evilcraft";
     public static final String MOD_FORESTRY = "forestry";
     public static final String MOD_CRAFTTWEAKER = "crafttweaker";
