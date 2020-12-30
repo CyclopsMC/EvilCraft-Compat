c87852a99eefb461a2ea2eca85ce0d72a3c2d63e
Ruben Taelman <rubensworks@gmail.com>
2020-12-30 15:20:25 +0100

    Fix crash when JEI is not present, Closes CyclopsMC/EvilCraft/#814

---
+++ src/main/java/org/cyclops/evilcraftcompat/RegistryEntriesCompat.java
@@ -0,0 +1,26 @@
+package org.cyclops.evilcraftcompat;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.ObjectHolder;
+
+/**
+ * @author rubensworks
+ */
+public class RegistryEntriesCompat {
+
+    @ObjectHolder("evilcraft:invigorating_pendant")
+    public static final Item ITEM_INVIGORATING_PENDANT = null;
+    @ObjectHolder("evilcraft:primed_pendant")
+    public static final Item ITEM_PRIMED_PENDANT = null;
+    @ObjectHolder("evilcraft:kineticator")
+    public static final Item ITEM_KINETICATOR = null;
+    @ObjectHolder("evilcraft:kineticator_repelling")
+    public static final Item ITEM_KINETICATOR_REPELLING = null;
+    @ObjectHolder("evilcraft:mace_of_destruction")
+    public static final Item ITEM_MACE_OF_DESTRUCTION = null;
+    @ObjectHolder("evilcraft:necromancer_staff")
+    public static final Item ITEM_NECROMANCER_STAFF = null;
+    @ObjectHolder("evilcraft:flesh_rejuvenated")
+    public static final Item ITEM_FLESH_REJUVENATED = null;
+
+}
--- src/main/java/org/cyclops/evilcraftcompat/modcompat/jei/JEIEvilCraftConfig.java
+++ src/main/java/org/cyclops/evilcraftcompat/modcompat/jei/JEIEvilCraftConfig.java
@@ -9,10 +9,8 @@ import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.IRecipeTransferRegistration;
 import mezz.jei.api.registration.ISubtypeRegistration;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.registries.ObjectHolder;
 import org.cyclops.evilcraft.RegistryEntries;
 import org.cyclops.evilcraft.client.gui.container.ContainerScreenBloodInfuser;
 import org.cyclops.evilcraft.client.gui.container.ContainerScreenExaltedCrafter;
@@ -25,6 +23,7 @@ import org.cyclops.evilcraft.inventory.container.ContainerExaltedCrafter;
 import org.cyclops.evilcraft.inventory.container.ContainerSanguinaryEnvironmentalAccumulator;
 import org.cyclops.evilcraft.tileentity.TileBloodInfuser;
 import org.cyclops.evilcraft.tileentity.TileSanguinaryEnvironmentalAccumulator;
+import org.cyclops.evilcraftcompat.RegistryEntriesCompat;
 import org.cyclops.evilcraftcompat.Reference;
 import org.cyclops.evilcraftcompat.modcompat.jei.bloodinfuser.BloodInfuserRecipeCategory;
 import org.cyclops.evilcraftcompat.modcompat.jei.bloodinfuser.BloodInfuserRecipeJEI;
@@ -41,35 +40,20 @@ import org.cyclops.evilcraftcompat.modcompat.jei.sanguinaryenvironmentalaccumula
 @JeiPlugin
 public class JEIEvilCraftConfig implements IModPlugin {
 
-    @ObjectHolder("evilcraft:invigorating_pendant")
-    public static final Item ITEM_INVIGORATING_PENDANT = null;
-    @ObjectHolder("evilcraft:primed_pendant")
-    public static final Item ITEM_PRIMED_PENDANT = null;
-    @ObjectHolder("evilcraft:kineticator")
-    public static final Item ITEM_KINETICATOR = null;
-    @ObjectHolder("evilcraft:kineticator_repelling")
-    public static final Item ITEM_KINETICATOR_REPELLING = null;
-    @ObjectHolder("evilcraft:mace_of_destruction")
-    public static final Item ITEM_MACE_OF_DESTRUCTION = null;
-    @ObjectHolder("evilcraft:necromancer_staff")
-    public static final Item ITEM_NECROMANCER_STAFF = null;
-    @ObjectHolder("evilcraft:flesh_rejuvenated")
-    public static final Item ITEM_FLESH_REJUVENATED = null;
-
     @Override
     public void registerItemSubtypes(ISubtypeRegistration subtypeRegistry) {
         SubtypeInterpreterActivatableFluidContainer subtypeInterpreter = new SubtypeInterpreterActivatableFluidContainer();
         subtypeRegistry.registerSubtypeInterpreter(RegistryEntries.ITEM_BLOOD_EXTRACTOR, subtypeInterpreter);
         subtypeRegistry.registerSubtypeInterpreter(RegistryEntries.ITEM_BLOOD_PEARL_OF_TELEPORTATION, subtypeInterpreter);
         subtypeRegistry.registerSubtypeInterpreter(RegistryEntries.ITEM_DARK_TANK, subtypeInterpreter);
-        subtypeRegistry.registerSubtypeInterpreter(ITEM_INVIGORATING_PENDANT, subtypeInterpreter);
-        subtypeRegistry.registerSubtypeInterpreter(ITEM_PRIMED_PENDANT, subtypeInterpreter);
-        subtypeRegistry.registerSubtypeInterpreter(ITEM_KINETICATOR, subtypeInterpreter);
-        subtypeRegistry.registerSubtypeInterpreter(ITEM_KINETICATOR_REPELLING, subtypeInterpreter);
+        subtypeRegistry.registerSubtypeInterpreter(RegistryEntriesCompat.ITEM_INVIGORATING_PENDANT, subtypeInterpreter);
+        subtypeRegistry.registerSubtypeInterpreter(RegistryEntriesCompat.ITEM_PRIMED_PENDANT, subtypeInterpreter);
+        subtypeRegistry.registerSubtypeInterpreter(RegistryEntriesCompat.ITEM_KINETICATOR, subtypeInterpreter);
+        subtypeRegistry.registerSubtypeInterpreter(RegistryEntriesCompat.ITEM_KINETICATOR_REPELLING, subtypeInterpreter);
         subtypeRegistry.registerSubtypeInterpreter(RegistryEntries.ITEM_MACE_OF_DISTORTION, subtypeInterpreter);
-        subtypeRegistry.registerSubtypeInterpreter(ITEM_MACE_OF_DESTRUCTION, subtypeInterpreter);
-        subtypeRegistry.registerSubtypeInterpreter(ITEM_NECROMANCER_STAFF, subtypeInterpreter);
-        subtypeRegistry.registerSubtypeInterpreter(ITEM_FLESH_REJUVENATED, subtypeInterpreter);
+        subtypeRegistry.registerSubtypeInterpreter(RegistryEntriesCompat.ITEM_MACE_OF_DESTRUCTION, subtypeInterpreter);
+        subtypeRegistry.registerSubtypeInterpreter(RegistryEntriesCompat.ITEM_NECROMANCER_STAFF, subtypeInterpreter);
+        subtypeRegistry.registerSubtypeInterpreter(RegistryEntriesCompat.ITEM_FLESH_REJUVENATED, subtypeInterpreter);
         subtypeRegistry.registerSubtypeInterpreter(RegistryEntries.ITEM_ENTANGLED_CHALICE, subtypeInterpreter);
     }
 
