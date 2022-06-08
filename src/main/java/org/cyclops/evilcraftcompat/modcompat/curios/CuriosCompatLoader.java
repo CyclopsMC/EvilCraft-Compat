c2b217eb8131ec4003edcef1d9e7de8e1f0aed34
Ruben Taelman <rubensworks@gmail.com>
2022-06-08 20:09:26 +0200

    Add Curios support for Exalted Crafter

--- src/main/java/org/cyclops/evilcraftcompat/modcompat/curios/CuriosCompatLoader.java
+++ src/main/java/org/cyclops/evilcraftcompat/modcompat/curios/CuriosCompatLoader.java
@@ -1,9 +1,7 @@
 package org.cyclops.evilcraftcompat.modcompat.curios;
 
 import net.minecraftforge.fml.InterModComms;
-import org.cyclops.cyclopscore.inventory.PlayerExtendedInventoryIterator;
 import org.cyclops.cyclopscore.modcompat.ICompatInitializer;
-import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.SlotTypePreset;
 
@@ -14,11 +12,9 @@ public class CuriosCompatLoader implements ICompatInitializer {
     @Override
     public void initialize() {
         // Register curios types
+        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BELT.getMessageBuilder().build());
+        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BODY.getMessageBuilder().build());
         InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.RING.getMessageBuilder().size(2).build());
         InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.NECKLACE.getMessageBuilder().build());
-
-        // Extend player iterator
-        PlayerExtendedInventoryIterator.INVENTORY_EXTENDERS
-                .add(playerEntity -> CuriosApi.getCuriosHelper().getEquippedCurios(playerEntity).orElse(null));
     }
 }
