a410127b7a35681ab64f20187bdd0a24efaab6b6
Ruben Taelman <rubensworks@gmail.com>
2024-07-21 06:21:22 +0200

    Fix rare race condition during startup, Closes CyclopsMC/EvilCraft#1034

--- src/main/java/org/cyclops/evilcraftcompat/modcompat/capabilities/CommonCapabilitiesCompatInitializer.java
+++ src/main/java/org/cyclops/evilcraftcompat/modcompat/capabilities/CommonCapabilitiesCompatInitializer.java
@@ -1,8 +1,8 @@
 package org.cyclops.evilcraftcompat.modcompat.capabilities;
 
 import org.cyclops.cyclopscore.modcompat.ICompatInitializer;
-import org.cyclops.evilcraft.EvilCraft;
 import org.cyclops.evilcraft.RegistryEntries;
+import org.cyclops.evilcraftcompat.EvilCraftCompat;
 
 /**
  * @author rubensworks
@@ -11,13 +11,13 @@ public class CommonCapabilitiesCompatInitializer implements ICompatInitializer {
     @Override
     public void initialize() {
         // Capabilities
-        EvilCraft._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_BLOOD_CHEST, new WorkerWorkingBlockEntityCompat<>());
-        EvilCraft._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_BLOOD_INFUSER, new WorkerWorkingBlockEntityCompat<>());
-        EvilCraft._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_COLOSSAL_BLOOD_CHEST, new WorkerWorkingBlockEntityCompat<>());
-        EvilCraft._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_SANGUINARY_ENVIRONMENTAL_ACCUMULATOR, new WorkerWorkingBlockEntityCompat<>());
-        EvilCraft._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_SPIRIT_FURNACE, new WorkerWorkingBlockEntityCompat<>());
-        EvilCraft._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_SPIRIT_REANIMATOR, new WorkerWorkingBlockEntityCompat<>());
-        EvilCraft._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_ENVIRONMENTAL_ACCUMULATOR, new WorkerEnvirAccBlockEntityCompat());
-        EvilCraft._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_BLOOD_INFUSER, new RecipeHandlerBloodInfuserBlockEntityCompat());
+        EvilCraftCompat._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_BLOOD_CHEST, new WorkerWorkingBlockEntityCompat<>());
+        EvilCraftCompat._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_BLOOD_INFUSER, new WorkerWorkingBlockEntityCompat<>());
+        EvilCraftCompat._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_COLOSSAL_BLOOD_CHEST, new WorkerWorkingBlockEntityCompat<>());
+        EvilCraftCompat._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_SANGUINARY_ENVIRONMENTAL_ACCUMULATOR, new WorkerWorkingBlockEntityCompat<>());
+        EvilCraftCompat._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_SPIRIT_FURNACE, new WorkerWorkingBlockEntityCompat<>());
+        EvilCraftCompat._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_SPIRIT_REANIMATOR, new WorkerWorkingBlockEntityCompat<>());
+        EvilCraftCompat._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_ENVIRONMENTAL_ACCUMULATOR, new WorkerEnvirAccBlockEntityCompat());
+        EvilCraftCompat._instance.getCapabilityConstructorRegistry().registerBlockEntity(RegistryEntries.BLOCK_ENTITY_BLOOD_INFUSER, new RecipeHandlerBloodInfuserBlockEntityCompat());
     }
 }
